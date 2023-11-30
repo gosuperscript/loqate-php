24952eea03d40c739c73f791dc018cfecbe558f4
Erik Gaal <me@erikgaal.nl>
2023-11-30 18:24:35 +0100

    fix: nullable address props

--- src/Data/Address.php
+++ src/Data/Address.php
@@ -9,48 +9,48 @@ final readonly class Address
 {
     public function __construct(
         public string $id,
-        public ?string $line1,
-        public ?string $line2,
-        public ?string $line3,
-        public ?string $line4,
-        public ?string $line5,
-        public ?string $street,
-        public ?string $company,
-        public ?string $buildingNumber,
-        public ?string $buildingName,
-        public ?string $postalCode,
-        public ?string $city,
-        public ?string $countryIso2,
-        public ?string $countryIso3,
-        public ?string $countryIsoNumber,
-        public ?string $countryName,
-        public stdClass $extra,
+        public ?string $line1 = null,
+        public ?string $line2 = null,
+        public ?string $line3 = null,
+        public ?string $line4 = null,
+        public ?string $line5 = null,
+        public ?string $street = null,
+        public ?string $company = null,
+        public ?string $buildingNumber = null,
+        public ?string $buildingName = null,
+        public ?string $postalCode = null,
+        public ?string $city = null,
+        public ?string $countryIso2 = null,
+        public ?string $countryIso3 = null,
+        public ?string $countryIsoNumber = null,
+        public ?string $countryName = null,
+        public stdClass $extra = new stdClass(),
     ) {
     }
 
     /**
      * @param array<string, mixed> $payload
-     * @param list<string>
+     * @param list<string> $fields
      */
     public static function fromArray(array $payload, array $fields): self
     {
         return new self(
             id: $payload['Id'],
-            line1: $payload['Line1'],
-            line2: $payload['Line2'],
-            line3: $payload['Line3'],
-            line4: $payload['Line4'],
-            line5: $payload['Line5'],
-            street: $payload['Street'],
-            company: $payload['Company'],
-            buildingNumber: $payload['BuildingNumber'],
-            buildingName: $payload['BuildingName'],
-            postalCode: $payload['PostalCode'],
-            city: $payload['City'],
-            countryIso2: $payload['CountryIso2'],
-            countryIso3: $payload['CountryIso3'],
-            countryIsoNumber: $payload['CountryIsoNumber'],
-            countryName: $payload['CountryName'],
+            line1: $payload['Line1'] ?? null,
+            line2: $payload['Line2'] ?? null,
+            line3: $payload['Line3'] ?? null,
+            line4: $payload['Line4'] ?? null,
+            line5: $payload['Line5'] ?? null,
+            street: $payload['Street'] ?? null,
+            company: $payload['Company'] ?? null,
+            buildingNumber: $payload['BuildingNumber'] ?? null,
+            buildingName: $payload['BuildingName'] ?? null,
+            postalCode: $payload['PostalCode'] ?? null,
+            city: $payload['City'] ?? null,
+            countryIso2: $payload['CountryIso2'] ?? null,
+            countryIso3: $payload['CountryIso3'] ?? null,
+            countryIsoNumber: $payload['CountryIsoNumber'] ?? null,
+            countryName: $payload['CountryName'] ?? null,
             extra: (object) collect($fields)
                 ->mapWithKeys(fn (string $field, int $index) => [$field => $payload[sprintf('Field%d', $index + 1)]])
                 ->all(),
